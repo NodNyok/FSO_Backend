71a4c65164151425b5bd143769291479d02233d2
Saara Rossi <saararossi@outlook.com>
2025-07-13 15:43:50 +0300

    uibuild

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require('express');
+const User = require('./models/user');
 const morgan = require('morgan');
 const app = express();
 const cors = require('cors');
@@ -15,6 +17,20 @@ morgan.token('content', function (req, res) {
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms')
 );
+
+// Generaalinen error handler
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
 
 let persons = [
   {
@@ -45,44 +61,67 @@ app.get('/', (request, response) => {
 
 // kaikkien henkilöiden sivu
 app.get('/api/persons', (request, response) => {
-  response.json(persons);
+  User.find({}).then((users) => response.json(users));
 });
 
-// infosivu
+// infosivu, saadaan tiedot mongodb kannasta mongoosen countDocuments -metodin avulla
 app.get('/info', (request, response) => {
-  response.send(
-    `<p>Phonebook has info for ${
-      persons.length
-    } people </p><p>${new Date()}</p>`
+  User.countDocuments({}).then((count) =>
+    response.send(
+      `<p>Phonebook has info for ${count} people </p><p>${new Date()}</p>`
+    )
   );
 });
 
 // yksittäisen henkilön sivu
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
+app.get('/api/persons/:id', (request, response, next) => {
+  User.findById(request.params.id)
+    .then((user) => {
+      if (user) {
+        response.json(user);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
+});
 
-  // katsotaan onko henkilö olemassa ja vastataan 404 jos ei ole
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
+// Käsitellään käyttäjän päivittäminen
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body;
+
+  User.findById(request.params.id)
+    .then((user) => {
+      if (!user) {
+        return response.status(404).end();
+      }
+
+      user.name = name;
+      user.number = number;
+
+      return user.save().then((updatedUser) => {
+        response.json(updatedUser);
+      });
+    })
+    .catch((error) => next(error));
 });
 
 // henkilön poisto
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-  response.status(204).end();
+app.delete('/api/persons/:id', (request, response, next) => {
+  User.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 // henkilön lisäys, logataan tässä myös pyynnön sisältö eli lisättävä henkilö
-app.post('/api/persons', morgan(':content'), (request, response) => {
+app.post('/api/persons', morgan(':content'), (request, response, next) => {
   // generoidaan random id
   const id = Math.floor(Math.random() * (50 - 10) + 10);
   const person = request.body;
-  // katsotaan onko duplikaatteja
+  // katsotaan onko duplikaatteja (ei toimi nyt, koska listan sijasta tiedot haetaan tietokannasta)
   const duplicates = persons.filter(
     (person) => person.name === request.body.name
   );
@@ -93,10 +132,27 @@ app.post('/api/persons', morgan(':content'), (request, response) => {
   } else if (duplicates.length !== 0) {
     return response.status(400).json({ error: 'name must be unique' });
   }
-  person.id = String(id);
-  persons = persons.concat(person);
-  response.json(person);
+
+  // Luodaan uusi käyttäjä mongodb -kantaan
+  const user = new User({
+    name: person.name,
+    number: person.number,
+    id: id,
+  });
+  user
+    .save()
+    .then((savedUser) => {
+      response.json(savedUser);
+    })
+    .catch((error) => next(error));
+
+  // Vanha tapa kovakoodattuun listaan lisäämiseen------------------------
+  // person.id = String(id);
+  // persons = persons.concat(person);
+  // response.json(person);
 });
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
